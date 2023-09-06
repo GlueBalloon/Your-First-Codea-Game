e0b87cbb7ef3e56c40c1a16f932aa39884f3cebe
Jesse Wonder Clark <jwc@jessewonderclark.com>
2023-09-06 00:49:04 -0400

    refactor pickupAction out of newBeesScreenVariables

--- BeesScreenHelpers.lua
+++ BeesScreenHelpers.lua
@@ -26,37 +26,41 @@ function newBeesScreenVariables()
     BSV.beeWidth = 58
     BSV.beeHeight = 29
     BSV.pickupAction = function(gameObject)
-        fadeVanish(gameObject)
-        BSV.score = BSV.score + 1
-        -- Animate the score text
-        tween(0.2, BSV.scoreSpecs, {size = 70, r = 255, g = 140, b = 0}, tween.easing.outQuad, function()
-            tween(0.2, BSV.scoreSpecs, {size = 60, r = 99, g = 255, b = 0}, tween.easing.inQuad)
-        end)
-        
-        if BSV.score >= 15 then
-            BSV.gameState = "won"
-            BSV.endTime = os.clock()
-        end
-        
-        local practicalX = gameObject.x + gameObject.hitboxOffsetX
-        local practicalY = gameObject.y + gameObject.hitboxOffsetY
-        local newBee = GameObject(asset.builtin.Platformer_Art.Battor_Flap_1, practicalX, practicalY, BSV.beeWidth * 0.5, BSV.beeHeight * 0.5)
-        newBee.movementModule = BuzzMotionModule(newBee, math.random(3, 9))
-        newBee.timeAlive = 0
-        -- Animate the bee growing to its full size
-        tween(1, newBee, {width = BSV.beeWidth, height = BSV.beeHeight}, {easing = tween.easing.linear})
-        -- Release all bees attached to the picked up bush
-        if BSV.bushBees[gameObject] then
-            for _, bee in ipairs(BSV.bushBees[gameObject]) do
-                --make the bee grow again
-                tween(1, bee, {width = BSV.beeWidth, height = BSV.beeHeight}, {easing = tween.easing.linear})
-            end
-            BSV.bushBees[gameObject] = nil
-        end
-        table.insert(BSV.enemies, newBee)
-        sound(SOUND_HIT, 1043)
+        handlePickup(BSV, gameObject)
     end
-    
     return BSV
 end
+
+function handlePickup(BSV, gameObject)
+    fadeVanish(gameObject)
+    BSV.score = BSV.score + 1
+    -- Animate the score text
+    tween(0.2, BSV.scoreSpecs, {size = 70, r = 255, g = 140, b = 0}, tween.easing.outQuad, function()
+        tween(0.2, BSV.scoreSpecs, {size = 60, r = 99, g = 255, b = 0}, tween.easing.inQuad)
+    end)
+    
+    if BSV.score >= 15 then
+        BSV.gameState = "won"
+        BSV.endTime = os.clock()
+    end
+    
+    local practicalX = gameObject.x + gameObject.hitboxOffsetX
+    local practicalY = gameObject.y + gameObject.hitboxOffsetY
+    local newBee = GameObject(asset.builtin.Platformer_Art.Battor_Flap_1, practicalX, practicalY, BSV.beeWidth * 0.5, BSV.beeHeight * 0.5)
+    newBee.movementModule = BuzzMotionModule(newBee, math.random(3, 9))
+    newBee.timeAlive = 0
+    -- Animate the bee growing to its full size
+    tween(1, newBee, {width = BSV.beeWidth, height = BSV.beeHeight}, {easing = tween.easing.linear})
+    -- Release all bees attached to the picked up bush
+    if BSV.bushBees[gameObject] then
+        for _, bee in ipairs(BSV.bushBees[gameObject]) do
+            --make the bee grow again
+            tween(1, bee, {width = BSV.beeWidth, height = BSV.beeHeight}, {easing = tween.easing.linear})
+        end
+        BSV.bushBees[gameObject] = nil
+    end
+    table.insert(BSV.enemies, newBee)
+    sound(SOUND_HIT, 1043)
+end
+
 
