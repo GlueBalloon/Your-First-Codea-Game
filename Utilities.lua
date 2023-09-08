0c27f5baec5dc0ad403bb9aae08d1cf9953a2533
Jesse Wonder Clark <jwc@jessewonderclark.com>
2023-09-08 11:37:53 -0400

    make randomPlus *ALSO INCLUDES INFO.PLIST FOR PREVIOUS FILES*

--- Utilities.lua
+++ Utilities.lua
@@ -17,6 +17,16 @@ function tableContains(table, element)
     return false
 end
 
+function randomPlus(min, max)
+    if min % 1 == 0 and max % 1 == 0 then
+        -- Both min and max are integers
+        return math.random(min, max)
+    else
+        -- At least one of min or max is a decimal
+        local offset = (max - min) * math.random()
+        return min + offset
+    end
+end
 
 function startupScreen()
     -- Read the progress variable from local storage
